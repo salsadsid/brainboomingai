f64ad8d7782fc943882bd53677fff678c7ecbd08
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2024-12-26 17:21:50 +0600

    mongoose type error fixed

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -6,7 +6,17 @@ if (!MONGO_URI) {
   throw new Error("Please define the MONGO_URI environment variable.");
 }
 
-let cached = global.mongoose;
+// Extend global object
+interface MongooseCache {
+  conn: mongoose.Connection | null;
+  promise: Promise<mongoose.Mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
@@ -22,11 +32,10 @@ async function dbConnect() {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose
-      .connect(MONGO_URI, opts)
-      .then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGO_URI, opts);
   }
-  cached.conn = await cached.promise;
+
+  cached.conn = (await cached.promise).connection;
   return cached.conn;
 }
 
